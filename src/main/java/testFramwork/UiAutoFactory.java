b9f728b401cde2e298966eb76adeff52b78ee1cf
east <east_yxd@163.com>
2020-07-03 16:58:43 +0800

    实现BasePage

---
+++ src/main/java/testFramwork/UiAutoFactory.java
@@ -0,0 +1,10 @@
+package testFramwork;
+
+/**
+ * @Classname UiAutoFactory
+ * @Description TODO
+ * @Date 2020/7/3 4:06 下午
+ * @Created by East
+ */
+public class UiAutoFactory {
+}
---
+++ src/test/java/testWeb/WebBasePageTest.java
@@ -0,0 +1,18 @@
+package testWeb;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * @Classname WebBasePageTest
+ * @Description TODO
+ * @Date 2020/7/3 2:30 下午
+ * @Created by East
+ */
+class WebBasePageTest {
+
+    @Test
+    void click() {
+    }
+}
