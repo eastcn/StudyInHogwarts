a2d5bb2308183e8eb5f115a305f67c56062518c8
east <east_yxd@163.com>
2020-07-22 23:59:40 +0800

    push

--- src/main/java/JavaDemo/StackDemo.java
+++ src/main/java/JavaDemo/StackDemo.java
@@ -1,10 +1,24 @@
 package JavaDemo;
 
+import java.util.Stack;
+
 /**
  * @Classname StackDemo
- * @Description TODO
+ * @Description 栈数据结构demo
  * @Date 2020/7/9 6:00 下午
  * @Created by East
  */
 public class StackDemo {
+    public static void push(Stack<Integer> intStack, int num) {
+        intStack.push(num);
+        System.out.println("add:" + intStack.add(1));
+        intStack.push(2);
+        intStack.addElement(3);
+        System.out.println("stack:" + intStack);
+    }
+
+    public static void main(String[] args) {
+        Stack<Integer> stack = new Stack<>();
+        push(stack, 0);
+    }
 }
